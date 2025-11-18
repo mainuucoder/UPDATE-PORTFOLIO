d0aeff39ace66f9980b67bdd656591b6ee166535
mutahidaniel2000@gmail.com <mutahidaniel2000@gmail.com>
2025-11-18 22:59:46 +0300

    initial commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Mail, MapPin } from "lucide-react";
-import heroImage from "@/assets/hero image.jpg";
+import heroImage from "@/assets/hero-bg.jpg";
 import TypingAnimation from "./TypingAnimation";
 import ParticleBackground from "./ParticleBackground";
 
